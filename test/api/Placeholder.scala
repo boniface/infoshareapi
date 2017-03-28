88f825966078bb9a03145186fbe8d5ae4708e62c
thuleboba hadebe <thulehadebe@outlook.com>
2017-03-28 19:26:06 +0200

    added placeholders

---
+++ test/api/Placeholder.scala
@@ -0,0 +1,8 @@
+package api
+
+/**
+  * Created by thuleh on 2017/03/28.
+  */
+class Placeholder {
+
+}
---
+++ test/services/Placeholder.scala
@@ -0,0 +1,8 @@
+package services
+
+/**
+  * Created by thuleh on 2017/03/28.
+  */
+class Placeholder {
+
+}
