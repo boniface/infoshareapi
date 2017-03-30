01c3f3ea94f1cbcf0823af381c9ae811bd81fcb8
thuleboba hadebe <2leradebe@gmail.com>
2017-03-30 16:40:35 +0200

    new changes

--- app/repositories/person/tables/PersonAddressTable.scala
+++ app/repositories/person/tables/PersonAddressTable.scala
@@ -1,6 +1,20 @@
 package repositories.person.tables
 
+import com.outworkers.phantom.dsl._
+import domain.person.PersonAddress
 
-class PersonAddressTable {
+
+class PersonAddressTable extends CassandraTable[PersonAddressTable, PersonAddress] {
+
+  object personId extends StringColumn(this) with PartitionKey
+  object addressTypeId extends StringColumn(this) with PrimaryKey
+  object id extends StringColumn(this) with PrimaryKey
+  object description extends StringColumn(this)
+  object postalCode extends StringColumn(this)
+
+  object date extends DateColumn(this)
+
+  object state extends StringColumn(this)
 
 }
+
