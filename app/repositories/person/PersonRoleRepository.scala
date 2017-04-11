99b67a39aaa957e8e33e47b66d2965e20dccbb27
thuleboba hadebe <2leradebe@gmail.com>
2017-04-11 16:45:05 +0200

    fixed person demographics table error

---
+++ app/repositories/person/PersonRoleRepository.scala
@@ -0,0 +1,17 @@
+package repositories.person
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.person.tables.PersonRoleTableImpl
+
+
+class PersonRoleDatabase (override val connector: KeySpaceDef) extends Database[PersonRoleDatabase](connector) {
+  object PersonRoleTable extends PersonRoleTableImpl with connector.Connector
+}
+
+object personRoleDatabase extends PersonRoleDatabase(DataConnection.connector)
+
+trait PersonRoleRepository {
+  def  database = personRoleDatabase
+}
+
