d6c226a354e6a99b73caf8165e5750f5f62f9ebf
thuleboba hadebe <2leradebe@gmail.com>
2017-04-12 17:29:34 +0200

    left with test cases for repositories

--- test/services/PersonServiceTest.scala
+++ test/services/PersonServiceTest.scala
@@ -12,36 +12,46 @@ class PersonServiceTest extends PlaySpec{
   "PersonSrevice # getPerson" should {
     "find a person given an organisation and ID" in {
 
-      val personRecord = Person("HBC", "1", "John", "Johhny",
-        "john@example.com", "Doe", "*",
-        true, false, true, true, "the state")
+      val personRecord = Person("HBC", "2", "John", "Johhny",
+        "john@example.com", "Doe", "password",
+        true, false, true, true, "ACTIVE")
 
       val personService = PersonService
       personService.save(personRecord)
 
-      val person = personService.getPerson(org = "HBC", id = "1")
+      val person = personService.getPerson(org = "HBC", id = "2")
+
+      person mustBe personRecord
+      person.map {  o => o match {
+        case Some(x) => {
+          assert(x.org === "HBC")
+        }
+      }
+      }
+      println(person)
+      person.foreach(println)
     }
   }
 
 
-//  "PersonService #getById" should{
-//    "be true when the id exists" in{
-//
-//      val personAddress =  PersonAddress("Add1","123","1 Tenant Street","7925","ADT001", new Date(),"")
-//
-//      val personService = PersonAddressService
-//      personService.saveOrUpdate(personAddress)
-//      val addressType = personService.getValues("ADT001")
-//
-//      personAddress map {
-//        o => o match{
-//          case Some(x) =>{
-//            assert(x.description === "1 Tenant Street")
-//          }
-//        }
-//      }
-//    }
-//  }
+  //  "PersonService #getById" should{
+  //    "be true when the id exists" in{
+  //
+  //      val personAddress =  PersonAddress("Add1","123","1 Tenant Street","7925","ADT001", new Date(),"")
+  //
+  //      val personService = PersonAddressService
+  //      personService.saveOrUpdate(personAddress)
+  //      val addressType = personService.getValues("ADT001")
+  //
+  //      personAddress map {
+  //        o => o match{
+  //          case Some(x) =>{
+  //            assert(x.description === "1 Tenant Street")
+  //          }
+  //        }
+  //      }
+  //    }
+  //  }
 
 }
 
