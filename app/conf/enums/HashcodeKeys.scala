da3822cced9fc7196bcf74baa985a8257284869d
THULEBONA HADEBE <2leradebe@gmail.com>
2017-04-18 15:54:40 +0200

    created enum folder

---
+++ app/conf/enums/HashcodeKeys.scala
@@ -0,0 +1,12 @@
+package conf.enums
+
+/**
+  * Created by hashcode on 2016/10/15.
+  */
+object HashcodeKeys  extends  Enumeration{
+  val RSSFEED=Value("RSS")
+  val GeocodingAPI, CDN_URL, MAILORG, DEALLOCATED, ALLOCATED, DISABLED, ACTIVE, INACTIVE, MARGINTOKENKEY = Value
+
+
+
+}
---
+++ app/conf/enums/RolesID.scala
@@ -0,0 +1,8 @@
+package conf.enums
+
+
+object RolesID extends Enumeration{
+  val ORG_ADMIN= Value("ADMINISTRATOR")
+  val ADMIN, FUNDER, ACCOUNTANT, MANAGER, READER, HELPDESK, FINANCE , ADVISOR = Value
+
+}
